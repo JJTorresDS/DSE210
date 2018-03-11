5e396e3efc8dee4e99a1a66dd744ff9a7bde792d
Jonas Torres <jonasjere@gmail.com>
2018-03-11 06:08:14 -0300

    Minor changes to largest_face function

--- week5.py
+++ week5.py
@@ -31,59 +31,12 @@ def largest_face(f, x_max):
         if die >= x_max:
             A = np.array([i for i in range(1,die+1)])
             e = sum(A<=x_max)
-            p1 *= e/die
-            p2 *= (e-1)/die
+            p1 *= e/float(die)
+            p2 *= (e-1)/float(die)
         
     return p1-p2
 
-def largest_face(f, x_max):
-    
-    def product(*args, repeat=1):
-        # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
-        # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
-        pools = [tuple(pool) for pool in args] * repeat
-        result = [[]]
-        for pool in pools:
-            result = [x+[y] for x in result for y in pool]
-        for prod in result:
-            yield tuple(prod)
-    
-    #Base case: Only one die
-    if type(f)==int:
-        # Posibles combinaciones del dado
-        A = {i for i in range(1,f[0]+1)} 
-        # Si el número máximo esta en el dado entonces prob=1/|A|
-        if x_max in A:
-            return 1/float(len(A))
-        else:
-            return 0
-  
-    else:
-        # Por cada dado en F
-        
-        A = {i for i in range(1,max(f)+1)} 
-        
-        temp = set(product(A, repeat=len(f)))
-        
-        temp2 = set()     
-        for x in temp:
-            boolVector = []
-            counter = 0
-            for i in x:         
-                boolVector.append(i > f[counter])
-                counter += 1
-                   
-            if sum(boolVector) == 0:
-                temp2.add(x)
-        
-        temp3 = set()     
-        for x in temp2:
-                               
-            if max(x)==x_max:
-                temp3.add(x)
-                    
-        
-        return len(temp3)/float(len(temp2))     
+
 
 # Face Sum
 def face_sum(m, s):
